83104223f6605b9afb81657c3923f88508bd804e
isabelaritacs <00001124535512SP@al.educacao.sp.gov.br>
2024-09-17 16:37:57 -0300

    adicionando arquivo js

---
+++ sketch.js
@@ -0,0 +1,33 @@
+let cor;
+
+let circuloX;
+let circuloY;
+function setup() {
+  createCanvas(400, 400);
+  background("white");
+  cor=color(random (0,255),random (0,255),random(0,255),random(0,100));
+  
+  
+  circuloX=[0, 0];
+  circuloY=[random(0,200), random(0,200)];
+}
+
+function draw() {
+  fill(cor);
+  
+  for(let contador in circuloX){
+    circle(circuloX[contador],circuloY[contador],30);
+    circuloX[contador]+=random(0,3);
+    circuloY[contador]+=random(-3,3);
+    
+    if(circuloX[contador]>=width){
+      circuloX[contador]=0;
+      circuloY[contador]+=random(0,200);
+    }
+  }
+  
+  if (mouseIsPressed){
+    cor = color(random(0,255), random(0,255), random(0,255), random(0,100));
+  }
+}
+//parei na 12
